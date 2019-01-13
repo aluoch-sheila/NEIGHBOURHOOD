78b436112a105235a882b6d9c49a2410e83b89b3
aluoch-sheila <aluochsheila1999@gmail.com>
2019-01-13 10:38:43 +0300

    defined a class for the sign up form

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -1,13 +1,13 @@
-# from django import forms
-# from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.models import User
-# from .models import Neighbourhood,Business,Profile,Posts,Comments
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Neighbourhood,Business,Profile,Posts,Comments
 
-# class SignupForm(UserCreationForm):
-#     email = forms.EmailField(max_length=200, help_text='Required')
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email', 'password1', 'password2')
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 # class AddHoodForm(forms.ModelForm):
 #   '''
