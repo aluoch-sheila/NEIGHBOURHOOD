14e4f58e1bd9f2b49cc99291457be63267dfcfc7
aluoch-sheila <aluochsheila1999@gmail.com>
2019-01-13 10:13:56 +0300

    defined the loging in function

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,129 +1,129 @@
-# from django.http import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
-# from django.shortcuts import render, redirect
-# from django.contrib.auth import login, authenticate
-# from django.contrib.auth.forms import UserCreationForm
-# from django.contrib import messages
-# from .forms import SignupForm,AddHoodForm,AddBusinessForm,UpdateProfileForm,PostForm
-# from django.contrib.sites.shortcuts import get_current_site
-# from django.utils.encoding import force_bytes, force_text
-# from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-# from django.template.loader import render_to_string
-# from .tokens import account_activation_token
-# from django.contrib.auth.models import User
-# from django.core.mail import EmailMessage
-# from .models import Neighbourhood,Business,Profile,Join,Posts,Comments
-# import datetime as dt
-# from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import SignupForm,AddHoodForm,AddBusinessForm,UpdateProfileForm,PostForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from .models import Neighbourhood,Business,Profile,Join,Posts,Comments
+import datetime as dt
+from django.contrib.auth.decorators import login_required
 
-# # Create your views here.
-# @login_required(login_url='/accounts/login/')
-# def index(request):
-#   '''
-#   View function that renders the homepage
-#   '''
-#   neighbourhoods = Neighbourhood.objects.all()
-#   return render(request,'index.html',locals())
-#   # if request.user.is_authenticated:
-#   #   if Join.objects.filter(user_id = request.user).exists():
-#   #     neighbourhoods = Neighbourhood.objects.get(pk = request.user.join.hood_id.id)
-#   #     posts = Posts.objects.filter(hood = request.user.join.hood_id.id)
-#   #     businesses = Business.objects.filter(neighbourhood=request.user.join.hood_id.id)
-#   #     return render(request,'index.html',locals())
+# Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+  '''
+  View function that renders the homepage
+  '''
+  neighbourhoods = Neighbourhood.objects.all()
+  return render(request,'index.html',locals())
+  # if request.user.is_authenticated:
+  #   if Join.objects.filter(user_id = request.user).exists():
+  #     neighbourhoods = Neighbourhood.objects.get(pk = request.user.join.hood_id.id)
+  #     posts = Posts.objects.filter(hood = request.user.join.hood_id.id)
+  #     businesses = Business.objects.filter(neighbourhood=request.user.join.hood_id.id)
+  #     return render(request,'index.html',locals())
 
-#   #   else:
-#   #     neighbourhoods = Neighbourhood.objects.all()
-#   #     return render(request,'index.html',locals())
-#   # else:
-#   #   neighbourhoods = Neighbourhood.objects.all()
-#   #   return render(request,'index.html',locals())
+  #   else:
+  #     neighbourhoods = Neighbourhood.objects.all()
+  #     return render(request,'index.html',locals())
+  # else:
+  #   neighbourhoods = Neighbourhood.objects.all()
+  #   return render(request,'index.html',locals())
 
-# @login_required(login_url='/accounts/login/')
-# def search_business(request):
+@login_required(login_url='/accounts/login/')
+def search_business(request):
   
-#     if 'business' in request.GET and request.GET["business"]:
-#         search_term = request.GET.get("business")
-#         businesses = Business.objects.filter(name__icontains = search_term,hood = request.user.join.hood_id.id)
-#         searched_businesses = Business.search_by_title(search_term)
-#         message = f"{search_term}"
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        businesses = Business.objects.filter(name__icontains = search_term,hood = request.user.join.hood_id.id)
+        searched_businesses = Business.search_by_title(search_term)
+        message = f"{search_term}"
 
-#         return render(request, 'search.html',locals())
+        return render(request, 'search.html',locals())
 
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',locals())
+# def signup(request):
+#     if request.method == 'POST':
+#         form = SignupForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.is_active = False
+#             user.save()
+#             current_site = get_current_site(request)
+#             mail_subject = 'Activate your Hoodwatch account.'
+#             message = render_to_string('registration/activate_account.html', {
+#                 'user': user,
+#                 'domain': current_site.domain,
+#                 'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+#                 'token':account_activation_token.make_token(user),
+#             })
+#             to_email = form.cleaned_data.get('email')
+#             email = EmailMessage(
+#                         mail_subject, message, to=[to_email]
+#             )
+#             email.send()
+#             return HttpResponse('Please confirm your email address to complete the registration')
 #     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'search.html',locals())
-# # def signup(request):
-# #     if request.method == 'POST':
-# #         form = SignupForm(request.POST)
-# #         if form.is_valid():
-# #             user = form.save(commit=False)
-# #             user.is_active = False
-# #             user.save()
-# #             current_site = get_current_site(request)
-# #             mail_subject = 'Activate your Hoodwatch account.'
-# #             message = render_to_string('registration/activate_account.html', {
-# #                 'user': user,
-# #                 'domain': current_site.domain,
-# #                 'uid':urlsafe_base64_encode(force_bytes(user.pk)),
-# #                 'token':account_activation_token.make_token(user),
-# #             })
-# #             to_email = form.cleaned_data.get('email')
-# #             email = EmailMessage(
-# #                         mail_subject, message, to=[to_email]
-# #             )
-# #             email.send()
-# #             return HttpResponse('Please confirm your email address to complete the registration')
-# #     else:
-# #         form = SignupForm()
-# #     return render(request, 'registration/signup.html', {'form': form})
+#         form = SignupForm()
+#     return render(request, 'registration/signup.html', {'form': form})
 
 
-# # def activate(request, uidb64, token):
-# #     try:
-# #         uid = force_text(urlsafe_base64_decode(uidb64))
-# #         user = User.objects.get(pk=uid)
-# #     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-# #         user = None
-# #     if user is not None and account_activation_token.check_token(user, token):
-# #         user.is_active = True
-# #         user.save()
-# #         login(request, user)
-# #         return HttpResponse('Thank you for your email confirmation. Now you can login your account.''<a href="/accounts/login/"> click here </a>')
-# #     else:
-# #         return HttpResponse('Activation link is invalid!')
+# def activate(request, uidb64, token):
+#     try:
+#         uid = force_text(urlsafe_base64_decode(uidb64))
+#         user = User.objects.get(pk=uid)
+#     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+#         user = None
+#     if user is not None and account_activation_token.check_token(user, token):
+#         user.is_active = True
+#         user.save()
+#         login(request, user)
+#         return HttpResponse('Thank you for your email confirmation. Now you can login your account.''<a href="/accounts/login/"> click here </a>')
+#     else:
+#         return HttpResponse('Activation link is invalid!')
 
-# @login_required(login_url='/accounts/login/')
-# def add_hood(request):
-# 	'''
-# 	View function that enables users to add hoods
-# 	'''
-# 	if request.method == 'POST':
-# 		form = AddHoodForm(request.POST)
-# 		if form.is_valid():
-# 			neighbourhood = form.save(commit = False)
-# 			neighbourhood.user = request.user
-# 			neighbourhood.save()
-# 			messages.success(request, 'You Have succesfully created a hood.You may now join your neighbourhood')
-# 			return redirect('home')
+@login_required(login_url='/accounts/login/')
+def add_hood(request):
+	'''
+	View function that enables users to add hoods
+	'''
+	if request.method == 'POST':
+		form = AddHoodForm(request.POST)
+		if form.is_valid():
+			neighbourhood = form.save(commit = False)
+			neighbourhood.user = request.user
+			neighbourhood.save()
+			messages.success(request, 'You Have succesfully created a hood.You may now join your neighbourhood')
+			return redirect('home')
 
-# 	else:
-# 		form = AddHoodForm()
-# 		return render(request,'add_hood.html',locals())
+	else:
+		form = AddHoodForm()
+		return render(request,'add_hood.html',locals())
 
-# @login_required(login_url='/accounts/login/')
-# def join_hood(request,hood_id):
-# 	'''
-# 	View function that enables users join a hood
-# 	'''
-# 	neighbourhood = Neighbourhood.objects.get(pk = hood_id)
-# 	if Join.objects.filter(user_id = request.user).exists():
+@login_required(login_url='/accounts/login/')
+def join_hood(request,hood_id):
+	'''
+	View function that enables users join a hood
+	'''
+	neighbourhood = Neighbourhood.objects.get(pk = hood_id)
+	if Join.objects.filter(user_id = request.user).exists():
 		
-# 		Join.objects.filter(user_id = request.user).update(hood_id = neighbourhood)
-# 	else:
+		Join.objects.filter(user_id = request.user).update(hood_id = neighbourhood)
+	else:
 		
-# 		Join(user_id=request.user,hood_id = neighbourhood).save()
+		Join(user_id=request.user,hood_id = neighbourhood).save()
 
-# 	messages.success(request, 'Success! You have succesfully joined this Neighbourhood ')
-# 	return redirect('index')
+	messages.success(request, 'Success! You have succesfully joined this Neighbourhood ')
+	return redirect('index')
 
 # def home(request):
 #   '''
